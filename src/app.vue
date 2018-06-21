cd0b38bc755129809c29ef7952e20167c4df7b50
Admin <admin@AdmindeMacBook-Pro-40.local>
2018-06-21 14:43:31 +0800

    webpack vue hello_world

---
+++ src/app.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <div id="test">{{text}}</div>
+</template>
+
+<script>
+    export default {
+
+        data(){
+            return{
+                text:'abcddd'
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+    #test{
+        color:green;
+    }
+
+</style>
